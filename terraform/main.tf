c35f7a2f892bb8dd618a1533f8bdb2b6ab335df3
Raoni Leite <raoni.leite@outlook.com>
2025-10-02 12:23:23 -0300

    Fix AWS Cognito User Pool Client

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,6 +88,12 @@ resource "aws_cognito_user_pool_client" "client" {
   name             = "vpc-api-client"
   user_pool_id     = aws_cognito_user_pool.users.id
   generate_secret  = false
+
+  explicit_auth_flows = [
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
 }
 
 # --- Test User ---
