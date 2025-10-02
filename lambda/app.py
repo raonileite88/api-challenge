0f40cc2ad5b40152e13bb4a9ff328ae59b7e1a33
Raoni Leite <raoni.leite@outlook.com>
2025-10-02 19:00:27 -0300

    Change Lambda to get logs

--- lambda/app.py
+++ lambda/app.py
@@ -11,7 +11,7 @@ table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
     # --- DEBUG: log the incoming event ---
-    print("Raw event:")
+    print("=== Incoming Event ===")
     print(json.dumps(event, indent=2))
 
     # --- Extract HTTP method and path ---
