c2e97e6188fd4e33213430b338834afa66989505
Raoni Leite <raoni.leite@outlook.com>
2025-10-02 20:28:15 -0300

    Remove DynamoDB from output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,10 +6,6 @@ output "api_endpoint" {
   value = aws_apigatewayv2_api.vpc_api.api_endpoint
 }
 
-output "dynamodb_table" {
-  value = aws_dynamodb_table.vpc_table.name
-}
-
 output "cognito_user_pool_id" {
   value = aws_cognito_user_pool.users.id
 }
